83a7345dfa51b53bd4a8ff35fd883b93e6903946
Olsbeetle <lesueuroliver2@gmail.com>
2024-11-29 16:22:42 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+player.onChat("jump", function () {
+	
+})
+player.onChat("farm", function () {
+    agent.teleportToPlayer()
+    agent.setItem(SEEDS, 16, 1)
+    agent.setAssist(DESTROY_OBSTACLES, true)
+    for (let index = 0; index < 4; index++) {
+        for (let index = 0; index < 4; index++) {
+            agent.move(LEFT, 1)
+            agent.till(FORWARD)
+            agent.place(FORWARD)
+            agent.move(BACK, 1)
+            agent.move(RIGHT, 1)
+        }
+    }
+})
